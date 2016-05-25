742902ee94bb2fece6178caf300cf29636836361
Colin Davis <nilocsivad@aol.com>
2016-05-25 19:49:49 +0800

    remove contact

--- app/src/main/java/com/iam_vip/colin/emptyview4/MainActivity.java
+++ app/src/main/java/com/iam_vip/colin/emptyview4/MainActivity.java
@@ -1,9 +1,21 @@
 package com.iam_vip.colin.emptyview4;
 
+import android.content.ContentProviderOperation;
+import android.content.ContentUris;
+import android.content.OperationApplicationException;
+import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,14 +34,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onResume() {
         super.onResume();
 
+        TypedArray actionbarSizeTypedArray = this.obtainStyledAttributes(new int[]{
+                android.R.attr.actionBarSize
+        });
+        int h = actionbarSizeTypedArray.getDimensionPixelOffset(0, 0);
+        tvContactInfo.setHeight(h);
 
+        tvContactInfo.setTextSize(h / 4);
+
+        this.resetContactInfo();
+    }
+
+    private void resetContactInfo() {
+        Cursor cur = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        int count = cur.getCount();
+        cur.close();
+        tvContactInfo.setText("Count contact is " + count);
     }
 
 
-
-
     private void initComponents() {
-        tvContactInfo = (TextView)this.findViewById(R.id.tv_contact_info);
+        tvContactInfo = (TextView) this.findViewById(R.id.tv_contact_info);
 
         tvCleanContact = (TextView) this.findViewById(R.id.tv_clean_contact);
         tvCleanContact.setOnClickListener(this);
@@ -44,7 +69,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_clean_contact:
+                //
+                Cursor cur = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+                //
+                ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>(cur.getCount());
+                //
+                while (cur.moveToNext()) {
+                    long _ID = cur.getLong(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    ops.add(ContentProviderOperation.newDelete(ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI,_ID)).build());
+                }
+                //
+                if (ops.size()>0) {
+                    try {
+                        this.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+                        this.showSuccessClean();
+                    } catch (RemoteException | OperationApplicationException e) {
+                        e.printStackTrace();
+                        Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    this.showSuccessClean();
+                }
+                cur.close();
+                //
+                this.resetContactInfo();
                 break;
         }
     }
+
+    private void showSuccessClean() {
+        Toast.makeText(this, "Success to clean contacts!", Toast.LENGTH_LONG).show();
+    }
+
+
 }
